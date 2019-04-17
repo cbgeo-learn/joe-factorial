96dc77eec95e9114cf6441018806a70485916294
Joseph Vantassel <jvantassel@utexas.edu>
2019-04-17 11:58:55 -0500

    Change while to for

--- Factorial.cpp
+++ Factorial.cpp
@@ -4,18 +4,14 @@
  * of a number and returns the result*/
 
 int main() {
-	int x={}; //Initialize input variable x	
+	int x; //Initialize input variable x	
 	std::cout << "Please enter a positive integer: ";
 	std::cin >> x;	
 
-	int y={1}; //Initialize ouput variable y
-	int z={2}; //Start at lowest value of factorial
-	while (z<=x)
+	int y = 1; //Initialize ouput variable y
+	for (int z = 1; z <= x; ++z) //Loop from lowest value of z to x
 		{
 		y = y*z;
-		++z;
 		}
-
 	std::cout << "The factorial of " << x << " is " << y << '\n';
-	return 0;
 }
