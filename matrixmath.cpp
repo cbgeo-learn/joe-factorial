7e9130063313b8d304034237261e2ae1d298773e
Joseph Vantassel <jvantassel@utexas.edu>
2019-04-30 10:23:51 -0500

    Matrix add and multiply working with oper overload

--- matrixmath.cpp
+++ matrixmath.cpp
@@ -1,36 +1,32 @@
 #include <iostream>
 
-int dotproduct(int* a, int* b, int length)
-{
-	int dot=0;	
-	for (int id=0; id<length; ++id)
-		dot+= (a[id]) * (b[id]);
-	return dot;		
-}
-
-
 class Matrix
 {
-private:
-	int rows;
-	int cols;
-	int **vals;
-
 public:
-	Matrix( int numrows, int numcols)
+	// Constructor, call init to make 2D array and set to set it.
+	Matrix(int numrows, int numcols)
 	{
 		rows = numrows;
 		cols = numcols;
-		vals = new int*[rows];
-		for (int rid=0; rid<rows; ++rid)
-		{
-			vals[rid] = new int[cols];
-			for (int cid=0; cid<cols; ++cid)
-				vals[rid][cid] = 0;
-		}
+		set(init(rows, cols));
 	}
 
-	void set()
+	// Make 2D array given rows and cols, and initialize to optional arg value.
+	int** init( int numrows, int numcols, int value=0)
+	{	
+		int **temp;
+                temp = new int*[rows];
+                for (int rid=0; rid<rows; ++rid)
+                {
+                        temp[rid] = new int[cols];
+                        for (int cid=0; cid<cols; ++cid)
+                                temp[rid][cid] = value;
+                }
+		return temp;
+	}	
+	
+	// Allow user defined set, where will loop through values of matrix
+	void setusr()
 	{
 		for (int rid=0; rid<rows; ++rid)
 		{
@@ -41,109 +37,102 @@ public:
 			}
 		}				
 	}
-	
+
+	void set(int** valstoset) {vals=valstoset;}
+
+	int getrows() {return rows;}
+
+	int getcols() {return cols;}
+
+	// Print matrix for visualization	
 	void print()
 	{
 		for (int rid=0; rid<rows; ++rid)
 		{
 			for (int cid=0; cid<cols; ++cid)
                      	std::cout<<vals[rid][cid]<<" ";
-		std::cout<<"\n"; 
-                }
-        }
+		std::cout<<"\n";
+		}
+	}
 
+	// Destructor to handle dynamic array
 	~Matrix()
 	{
 		for (int rid=0; rid<rows; ++rid)
 			delete[] vals[rid];
 		delete[] vals;
 	}
-/*
-	void add(Matrix &A, Matrix &B)
-	{
-		for (int rid=0; rid<rows; ++rid)
-		{
-			for (int cid=0; cid<cols; ++cid)
-				*vals[rid][cid] = *A.vals[rid][cid] + *B.vals[rid][cid];
-		}
-	}
 
-	
-	Matrix operator + (Matrix &B)
-        {
-		Matrix temp;
-		temp.init();
-                for (int rid=0; rid<rows; ++rid)
-                {
-                        for (int cid=0; cid<cols; ++cid)
-                              *temp.vals[rid][cid] = *vals[rid][cid] + *B.vals[rid][cid];
-                }
+	// Overload + opperator so that Matrix A + Matrix B will return vals for a Matrix C
+	int** operator + (Matrix &B)
+	{
+		int** temp;
+		temp=init(rows,cols);
+
+		if ((rows==B.getrows()) && (cols==B.getcols())) //If A and B are same size
+		{
+                	for (int rid=0; rid<rows; ++rid)
+			{
+                        	for (int cid=0; cid<cols; ++cid)
+					temp[rid][cid] = vals[rid][cid] + B.vals[rid][cid];
+			}
+		}
+		else //If A and B are different sizes
+			std::cout<<"Operation not allowed rows and cols must match";
 		return temp;
 	}	
 
-	Matrix operator * (Matrix &B)
+	// Overload * operator so that Matrix A * Matrix B will return vals for a Matrix C
+	int** operator * (Matrix &B)
 	{
-		Matrix temp;
-		temp.init();
+		int **temp;
+		temp=init(rows,B.getcols());
+
+		if (cols==B.getrows())
+		{
 		for (int rid=0; rid<rows; ++rid)
 		{
 			for (int cid=0; cid<cols; ++cid)
 			{
-				for (int lid=0; lid<rows; ++lid)
-				{
-					*temp.vals[rid][cid]+=*vals[rid][lid] * *B.vals[lid][cid];
-				}
+				for (int dloc=0; dloc<cols; ++dloc)
+					temp[rid][cid]+=vals[rid][dloc] * B.vals[dloc][cid];
 			}
 		}
+		}
+		else
+			std::cout<<"Operation not allowed cols of A == rows of B";		
 		return temp;
 	}
 
-*/
+
+private:
+	int rows;
+        int cols;
+        int **vals;
 
 };
-
-
-
 
 int main()
 {
 	// Initialize A
 	Matrix A {2,2};
-	A.set();
+	A.setusr();
 	A.print();
 
 	// Initialize B
 	Matrix B {2,2};
-	B.set();
+	B.setusr();
 	B.print();
 
-/*
-	// Use class method to add matrices
-	Matrix C;
-	C.init();
-	C.add(A,B);
+	// Overload + operator	
+	Matrix C {2,2};
+	C.set(A + B);
 	C.print();
 
-	// Overload + operator	
-	Matrix D;
-	D.init();
-	D = A + B;
+	// Overload * operator - Matrix * Matrix
+	Matrix D {2,2}; 
+	D.set(A * B);
 	D.print();
 
-	// Test dot product
-	int a[] = {1,2};
-	int b[] = {3,4};
-
-	int c;
-	c = dotproduct(a,b,2);
-	std::cout<<c<<"\n";
-
-	// Overload * operator
-	Matrix E;
-	E.init();	
-	E = A * B;
-	E.print();
-	
-*/
+	// Overload * oeration - Matrix * Vector
 }
-
